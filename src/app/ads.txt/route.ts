83a4fad2c393d74f8a13f0965639263ad06fbcd4
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-15 05:44:05 +0000

    google is not detecting it

---
+++ src/app/ads.txt/route.ts
@@ -0,0 +1,13 @@
+
+import { NextResponse } from 'next/server';
+
+const ADS_TXT_CONTENT = 'google.com, pub-3042243846300811, DIRECT, f08c47fec0942fa0';
+
+export async function GET() {
+  return new NextResponse(ADS_TXT_CONTENT, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=86400, must-revalidate', // Cache for 24 hours
+    },
+  });
+}
