3dc2f971457efe0dec9210fc017715dd7aad0103
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-16 07:05:32 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/components/calculators/salary-calculator.tsx
+++ src/components/calculators/salary-calculator.tsx
@@ -20,6 +20,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
@@ -278,3 +279,5 @@ export default function SalaryCalculator() {
     </Card>
   );
 }
+
+    
