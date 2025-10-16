1ea989b22c988e7828c9a627eb1e9220ced3f4c2
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-16 15:25:46 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/top-nav.tsx
+++ src/components/layout/top-nav.tsx
@@ -58,7 +58,7 @@ export function TopNav() {
             <NavigationMenuList>
               {mainCategories.map(category => (
                  <NavigationMenuItem key={category.name}>
-                   <Link href={`/${category.name.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}-calculators`} legacyBehavior passHref>
+                   <Link href={`/${category.name.toLowerCase().replace(/ & /g, '-').replace(/ /g, '-')}-calculators`} passHref legacyBehavior>
                     <NavigationMenuLink asChild>
                       <a className={cn(navigationMenuTriggerStyle(), 'font-semibold')}>
                         {category.name}
