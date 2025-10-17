c051a3ab0ee481c60bae5a7137b4f327e41cc269
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-17 05:07:08 +0000

    Google Publisher Policies Next: Google Publisher Restrictions We are in

--- src/app/credit-card-payoff-calculator/page.tsx
+++
@@ -1,6 +0,0 @@
-// This file is a redirect to the new /debt-payoff-calculator page.
-import { redirect } from 'next/navigation';
-
-export default function CreditCardPayoffCalculatorRedirectPage() {
-    redirect('/debt-payoff-calculator');
-}
--- src/app/investment-calculator/page.tsx
+++
@@ -1,7 +0,0 @@
-
-// This file is a redirect to the new /compound-interest-calculator page.
-import { redirect } from 'next/navigation';
-
-export default function InvestmentCalculatorRedirectPage() {
-    redirect('/compound-interest-calculator');
-}
