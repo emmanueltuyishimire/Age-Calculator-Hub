8ca37b64606492d23a88e01ac9a459972909978c
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-10-17 12:04:45 +0000

    Try fixing this error: `Console Error: Invalid DOM property `class`. Did

--- src/app/matrix-calculator/page.tsx
+++ src/app/matrix-calculator/page.tsx
@@ -23,7 +23,7 @@ export default function MatrixCalculatorPage() {
 
         <section className="mt-12 space-y-8 animate-fade-in prose dark:prose-invert lg:prose-xl max-w-none">
             <p>A matrix, in a mathematical context, is a rectangular array of numbers, symbols, or expressions that are arranged in rows and columns. Matrices are often used in scientific fields such as physics, computer graphics, probability theory, statistics, calculus, numerical analysis, and more.</p>
-            <p>The dimensions of a matrix, A, are typically denoted as m × n. This means that A has m rows and n columns. When referring to a specific value in a matrix, called an element, a variable with two subscripts is often used to denote each element based on its position in the matrix. For example, given a<sub>i,j</sub>, where i = 1 and j = 3, a<sub>1,3</sub> is the value of the element in the first row and the third column of the given matrix.</p>
+            <p>The dimensions of a matrix, A, are typically denoted as m × n. This means that A has m rows and n columns. When referring to a specific value in a matrix, called an element, a variable with two subscripts is often used to denote each element based on its position in the matrix. For example, given a<sub>ij</sub>, where i = 1 and j = 3, a<sub>1,3</sub> is the value of the element in the first row and the third column of the given matrix.</p>
             <p>Matrix operations such as addition, multiplication, subtraction, etc., are similar to what most people are likely accustomed to seeing in basic arithmetic and algebra, but do differ in some ways, and are subject to certain constraints. Below are descriptions of the matrix operations that this calculator can perform.</p>
 
             <h2>Matrix addition</h2>
@@ -47,17 +47,16 @@ export default function MatrixCalculatorPage() {
             <h2>Inverse of a matrix</h2>
             <p>The inverse of a matrix A is denoted as A<sup>-1</sup>, where A<sup>-1</sup> is the inverse of A if the following is true:</p>
             <p className="text-center font-mono">A×A<sup>-1</sup> = A<sup>-1</sup>×A = I, where I is the identity matrix</p>
-            <p>An identity matrix is a square matrix with "1" across its diagonal, and "0" everywhere else. Only non-singular matrices (matrices whose determinants are not zero) have an inverse.</p>
+            <p>An identity matrix is a square matrix with "1" across its diagonal, and "0" everywhere else. The identity matrix is the matrix equivalent of the number "1." Only non-singular matrices (matrices whose determinants are not zero) have an inverse.</p>
 
             <h2>Reduced Row Echelon Form (RREF)</h2>
             <p>The Reduced Row Echelon Form (RREF) of a matrix is a specific form of a matrix obtained through a series of elementary row operations, a process known as Gauss-Jordan elimination. It is particularly useful for solving systems of linear equations. A matrix is in RREF if it satisfies all the following conditions:</p>
-            <ul class="list-disc pl-6">
+            <ul className="list-disc pl-6">
                 <li>The first non-zero number in each row (called the leading entry) is 1. This is known as the pivot.</li>
                 <li>Each leading 1 is the only non-zero number in its column.</li>
                 <li>Each leading 1 is in a column to the right of the leading 1 in the row above it.</li>
                 <li>All rows consisting entirely of zeros are at the bottom of the matrix.</li>
             </ul>
-             <p>This calculator can find the RREF of any matrix, which can then be interpreted to find the solution for a system of linear equations.</p>
 
         </section>
         <RelatedCalculators currentCategory="Math" currentHref="/matrix-calculator" />
